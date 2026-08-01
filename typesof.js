690446ef3d9ac42c4684bd3d701f670274642fda
shravani735 <arunsaomvanshi@gmail.com>
2026-08-01 10:24:36 +0000

    typeof.js

---
+++ typesof.js
@@ -0,0 +1,13 @@
+console.log(typeof 10);
+console.log(typeof "Diksha");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function(){});
+console.log(typeof Symbol());
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof new Date());t
